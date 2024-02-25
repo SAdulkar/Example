91fee93fd3ed297cfbc2fb5596814e75de101201
SAdulkar <158838139+SAdulkar@users.noreply.github.com>
2024-02-25 20:49:22 +0530

    Add files via upload

---
+++ myproject/employee/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+"""class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+    password1 = models.CharField(max_length=10)
+    password2 = models.CharField(max_length=10)
+    
+
+    def __str__(self):
+        return self.name
+"""
